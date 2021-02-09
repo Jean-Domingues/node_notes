7542a8d41522c1b1d5f2fc300623726e645e33fe
Jean Domingues <jeandomingues752@gmail.com>
2021-02-09 20:50:35 -0300

    cria novo repositório para node

---
+++ FileSystem/retornaArquivos.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises
+const path = require('path');
+
+async function readDir(rootDir){
+  rootDir = rootDir || path.resolve(__dirname);
+  const files = await fs.readdir(rootDir)
+  walk(files, rootDir)
+}
+
+async function walk(files, dirName){
+  for(let file of files){
+    const fullPath = path.resolve(dirName, file)
+    const stats = await fs.stat(fullPath)
+
+    if(/\.git/g.test(fullPath)) continue
+    if(/node_modules/g.test(fullPath)) continue
+
+    if(stats.isDirectory()){
+      readDir(fullPath)
+      continue
+    }
+
+    if(!/\.css/g.test(fullPath)) continue
+    console.log(fullPath)
+  }
+}
+
+readDir('C:/Users/jeand/desktop/pastas')
