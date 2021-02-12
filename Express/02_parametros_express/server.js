05c8e50b7a05396026a123e51330f4ffea3e170b
Jean Domingues <jeandomingues752@gmail.com>
2021-02-11 21:59:22 -0300

    Adicionando body da requisição

--- Express/02_parametros_express/server.js
+++ Express/02_parametros_express/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(
@@ -11,16 +12,16 @@ app.get('/', (req, res) => {
   );
 });
 
+// BODY = Corpo da minha requisição POST
 app.post('/', (req, res) => {
+  console.log(req.body);
   res.send('Enviado!');
 });
-
 
 // QUERY PARMS = /search ?lenght=20&filter=dateCreated
 
 // URL PARMS = /profile/:id  (http://site.com/profile/234)
 // parâmetro opcional  = /profile/:id?
-
 app.get('/testes/:id?', (req, res) => {
   console.log('URL params', req.params);
   console.log('Query params', req.query);
