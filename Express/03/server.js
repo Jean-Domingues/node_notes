3196fa5b0f4b59b4a64bdcf9a351b5629b8d0eb5
Jean Domingues <jeandomingues752@gmail.com>
2021-02-12 20:36:10 -0300

    Cria nova pasta

---
+++ Express/03/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+// BODY = Corpo da minha requisição POST
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Enviado!');
+});
+
+// QUERY PARMS = /search ?lenght=20&filter=dateCreated
+
+// URL PARMS = /profile/:id  (http://site.com/profile/234)
+// parâmetro opcional  = /profile/:id?
+app.get('/testes/:id?', (req, res) => {
+  console.log('URL params', req.params);
+  console.log('Query params', req.query);
+  res.send('oi');
+});
+
+app.listen(3000);
