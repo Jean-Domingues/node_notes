ad62e2d2eae6ff980a0b2d52b880335deab58d4d
Jean Domingues <jeandomingues752@gmail.com>
2021-02-15 21:16:41 -0300

    Trabalhando com views

--- Express/04/src/controllers/homeController.js
+++ Express/04/src/controllers/homeController.js
@@ -1,11 +1,13 @@
 exports.paginaInicial = (req, res) => {
-  res.send(
-    `<form action="/" method="POST">
-    Nome: <input type="text" name="nome">
-    <button>Enviar</button>
-    </form>
-    `
-  );
+  res.render('index')
+  
+  // res.send(
+  //   `<form action="/" method="POST">
+  //   Nome: <input type="text" name="nome">
+  //   <button>Enviar</button>
+  //   </form>
+  //   `
+  // );
 }
 
 exports.postRequest = (req, res) => {
